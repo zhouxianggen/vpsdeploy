0e89b808b92e6feb4a570244f7b28de1922a27ce
zhouxianggen <zhouxianggen@gmail.com>
2019-03-29 15:18:39 +0800

    update

--- init.sh
+++ init.sh
@@ -1,11 +1,5 @@
-vpsdeploy
-![](https://img.shields.io/badge/python%20-%203.7-brightgreen.svg)
-========
-> vps deploy tool 
+#!/bin/bash
 
-## `Run`
-1. 登陆vps
-```
 firewall-cmd --add-port=8899/tcp --permanent
 iptables -F
 
@@ -20,17 +14,3 @@ python setup.py install
 easy_install requests
 
 wget 'https://raw.githubusercontent.com/zhouxianggen/vpsdeploy/master/deploy.sh' -O deploy.sh
-```
-
-1. set vps name
-```
-echo name > .name
-```
-
-2. crontab
-```
-*/1 * * * * cd /root/pp && sh bohao.sh >log/bohao.log 2>&1
-*/1 * * * * cd /root/pp && python heartbeat.py 
-*/1 * * * * cd /root/pp && python pyproxy.py --log-file=log/pyproxy.log 
-```
-
